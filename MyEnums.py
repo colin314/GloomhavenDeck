6a04bef4b71f4060e5ce13048f494da3363662c2
Colin Anderson <colander@epic.com>
2024-05-11 11:52:35 -0500

    Create Effects enum

---
+++ MyEnums.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+
+class Effect(Enum):
+    CRITICAL = 1
+    MISS = 2
+    WOUND = 3
+    BRITTLE = 4
+    BANE = 5
+    POISON = 6
+    IMMOBILIZE = 7
+    DISARM = 8
+    INJURE = 9
+    STUN = 10
+    MUDDLE = 11
+    CURSE = 12
+    REGENERATE = 13
+    WARD = 14
+    INVISIBLE = 15
+    STRENGTHEN = 16
+    BLESS = 17
+    PUSH = 18
+    PULL = 19
+    PIERCE = 20
+    SHIELD = 21
+    RETALIATE = 22
+    HEAL = 23
+    GEN_FIRE = 24
+    GEN_ICE = 25
+    GEN_EARTH = 26
+    GEN_AIR = 27
+    GEN_LIGHT = 28
+    GEN_DARK = 29
+    GEN_ANY = 30
+    CUSTOM_1 = 31
+    CUSTOM_2 = 32
+    CUSTOM_3 = 33
