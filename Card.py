dcbdda34d71a7d4f9057a5df49f341384aed34d0
Colin Anderson <colander@epic.com>
2024-05-11 12:41:58 -0500

    Add unique IDs to cards

--- Card.py
+++ Card.py
@@ -1,8 +1,10 @@
 from MyEnums import Effect
+import uuid
 
 
 class Card:
     def __init__(self, modifier, rolling=False, *args):
+        self.id = uuid.uuid4()
         self.modifier = modifier
         self.rolling = rolling
         self.Effects = []
