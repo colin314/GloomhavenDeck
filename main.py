7444068131a79d4a7410766bca8674ff8456c418
Colin Anderson <colander@epic.com>
2024-06-07 17:41:18 -0500

    Initial implementation of attack deck program
    I'm using the cmd module. Attack N, reset, and curse N commands are
    implemented.

--- main.py
+++ main.py
@@ -1,11 +1,65 @@
 from Deck import Deck
+import cmd, sys
 
-f = open("BaseDeck.csv")
-lines = f.read().splitlines()
-
-for line in lines:
-    x = line.split(",", 2)
+deck = Deck("BaseDeck.csv", "DeckModifications.csv")
 
 
-deck = Deck("BaseDeck.csv")
-print([x.id for x in deck.Cards])
+class DeckProgram(cmd.Cmd):
+    intro = "Welcome to the automated Gloomhaven attack deck program."
+    prompt = "Input: "
+    file = None
+
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.deck = Deck("BaseDeck.csv", "DeckModifications.csv")
+
+    def do_attack(self, arg):
+        "Draw a card from the deck: attack 5"
+        self.deck.draw(*parse(arg))
+
+    def do_reset(self, arg):
+        "Shuffle the discard back into the attack deck: reset"
+        self.deck.reset()
+
+    def do_curse(self, arg):
+        "Add a n curses to your deck: curse"
+        if not arg:
+            arg = 1
+        self.deck.addCurse(int(arg))
+
+    def do_bye(self, arg):
+        "Stop recording, close the turtle window, and exit:  BYE"
+        print("Thank you for using the virtual attack deck")
+        self.close()
+        return True
+
+    # ----- record and playback -----
+    def do_record(self, arg):
+        "Save future commands to filename:  RECORD rose.cmd"
+        self.file = open(arg, "w")
+
+    def do_playback(self, arg):
+        "Playback commands from a file:  PLAYBACK rose.cmd"
+        self.close()
+        with open(arg) as f:
+            self.cmdqueue.extend(f.read().splitlines())
+
+    def precmd(self, line):
+        line = line.lower()
+        if self.file and "playback" not in line:
+            print(line, file=self.file)
+        return line
+
+    def close(self):
+        if self.file:
+            self.file.close()
+            self.file = None
+
+
+def parse(arg):
+    "Convert a series of zero or more numbers to an argument tuple"
+    return tuple(map(int, arg.split()))
+
+
+if __name__ == "__main__":
+    DeckProgram().cmdloop()
